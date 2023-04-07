5bb6400ca95e8ae373b841a26c228eee9861e479
firstagon <98339053+firstagon@users.noreply.github.com>
2023-04-07 21:04:00 +0500

    Added two animated coins

--- src/components/MainScreen/WelcomeBlock.js
+++ src/components/MainScreen/WelcomeBlock.js
@@ -1,11 +1,19 @@
 import { Fragment } from "react";
 import styles from "./WelcomeBlock.module.css";
+import bitcoinImage from "../../img/bitcoin.png";
+import etheriumImage from "../../img/etheriumcoin.png";
 
 const WelcomeBlock = () => {
   return (
     <Fragment>
-      <p className={styles.welcome}> What about coins </p>
-      <p> Самый наипростейший трекер различных криптовалют. </p>
+      <div className={styles.textArea}>
+        <img className={styles.coinImageLeft} src={bitcoinImage} alt="bitcoin coin" />
+        <div className={styles.textContainer}>
+          <p className={styles.welcome}> What about coins </p>
+          <p> Самый наипростейший трекер различных криптовалют. </p>
+        </div>
+        <img className={styles.coinImageRight} src={etheriumImage} alt="bitcoin coin" />
+      </div>
     </Fragment>
   );
 };
